9cd38bf29036841e2eabdb84b8f3d760e4660ac9
Gregory Nowakowski <gregory.nowakowski@gartner.com>
2018-04-01 13:40:08 -0400

    ga added

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -44,6 +44,16 @@ class MyDocument extends Document {
               }
             `}        
           </style>
+ 
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-116686642-1"></script>
+          <script dangerouslySetInnerHTML={{__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-116686642-1');
+          `}} />
+
         </Head>
         <body>
           {this.props.customValue}
