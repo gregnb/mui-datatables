c9768adf010cccdb9782e4156db49ff33e50dbad
gregn <gregnb@gmail.com>
2018-07-29 19:31:45 -0400

    update custom footer example

--- examples/customize-footer/CustomFooter.js
+++ examples/customize-footer/CustomFooter.js
@@ -4,17 +4,7 @@ import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import { withStyles } from "@material-ui/core/styles";
 
-const defaultToolbarSelectStyles = {
-  iconButton: {
-    marginRight: "24px",
-    top: "50%",
-    display: "inline-block",
-    position: "relative",
-    transform: "translateY(-50%)",
-  },
-  deleteIcon: {
-    color: "#000",
-  },
+const defaultFooterStyles = {
 };
 
 class CustomFooter extends React.Component {
@@ -35,4 +25,4 @@ class CustomFooter extends React.Component {
 
 }
 
-export default withStyles(defaultToolbarSelectStyles, { name: "CustomFooter" })(CustomFooter);
+export default withStyles(defaultFooterStyles, { name: "CustomFooter" })(CustomFooter);
