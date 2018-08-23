b040a9770f7df29500b3f0396168a90f4c79e6e1
Benjamin Bours <benjaminbours@MacBook-Pro-de-Benjamin.local>
2018-08-23 21:50:36 +0200

    fix issue #156

--- src/MUIDataTable.js
+++ src/MUIDataTable.js
@@ -696,9 +696,6 @@ class MUIDataTable extends React.Component {
       searchText,
     } = this.state;
 
-    if (!data.length) {
-      return false;
-    }
 
     const rowCount = this.options.count || displayData.length;
 
