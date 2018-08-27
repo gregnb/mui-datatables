34c7e994834206093312958fd8125fff525b9e9e
Benjamin Bours <benjaminbours@MacBook-Pro-de-Benjamin.local>
2018-08-27 22:13:16 +0200

    add links

--- docs/utils/Menu.js
+++ docs/utils/Menu.js
@@ -17,15 +17,13 @@ const styles = theme => ({
 });
 
 const sandboxes = [
-    { name: "Simple", href: "https://codesandbox.io/s/new" },
-    { name: "Customize columns", href: "https://codesandbox.io/s/new" },
-    { name: "Customize footer", href: "https://codesandbox.io/s/new" },
-    { name: "Customize styling", href: "https://codesandbox.io/s/new" },
-    { name: "Customize toolbar", href: "https://codesandbox.io/s/new" },
-    { name: "Customize toolbarselect", href: "https://codesandbox.io/s/new" },
-    { name: "Resizable columns", href: "https://codesandbox.io/s/new" },
-    { name: "Serverside options", href: "https://codesandbox.io/s/new" },
-    { name: "Text localization", href: "https://codesandbox.io/s/new" },
+    { name: "Custom Component", href: "https://codesandbox.io/embed/xrvrzryjvp?autoresize=1&hidenavigation=1" },
+    { name: "Customize Columns", href: "https://codesandbox.io/embed/xowj5oj8w?autoresize=1&hidenavigation=1" },
+    { name: "Customize Footer", href: "https://codesandbox.io/embed/5z0w0w9jyk?autoresize=1&hidenavigation=1" },
+    { name: "Customize Styling", href: "https://codesandbox.io/embed/0ylq1lqwp0?autoresize=1&hidenavigation=1" },
+    { name: "Customize Toolbar", href: "https://codesandbox.io/embed/wy2rl1nyzl?autoresize=1&hidenavigation=1" },
+    { name: "Customize ToolbarSelect", href: "https://codesandbox.io/embed/545ym5ov6p?autoresize=1&hidenavigation=1" },
+    { name: "Resizable Columns", href: "https://codesandbox.io/embed/q8w3230qpj?autoresize=1&hidenavigation=1" },
 ];
 
 const Exemple = props => (
