46fd5aebcc42e232e89832279af376884a802c21
gregn <gregnb@gmail.com>
2018-10-21 14:26:22 -0400

    fix broken unit test

--- test/MUIDataTableToolbar.test.js
+++ test/MUIDataTableToolbar.test.js
@@ -151,7 +151,7 @@ describe("<MUIDataTableToolbar />", function() {
   });
 
   it("should download CSV when calling method handleCSVDownload", () => {
-    const shallowWrapper = shallow(<MUIDataTableToolbar columns={columns} data={data} options={options} />);
+    const shallowWrapper = shallow(<MUIDataTableToolbar columns={columns} displayData={data} data={data} options={options} />);
     const instance = shallowWrapper
       .dive()
       .dive()
