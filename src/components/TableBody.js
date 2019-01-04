afe43cc78d050869e19b83f7ab966520373fcb29
Pedro <pedroob221@gmail.com>
2019-01-04 03:40:13 +0000

    Replicate the data behaviour for server-side cases to allow the customization of noMatch in textLabels

--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -47,7 +47,7 @@ class TableBody extends React.Component {
   buildRows() {
     const { data, page, rowsPerPage, count } = this.props;
 
-    if (this.props.options.serverSide) return data;
+    if (this.props.options.serverSide) return data.length ? data : null;
 
     let rows = [];
     const totalPages = Math.floor(count / rowsPerPage);
