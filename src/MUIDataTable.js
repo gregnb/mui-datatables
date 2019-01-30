b77d6b5b99d8d7c48dc21d9a312f8f35694e0f1d
gregn <gregnb@gmail.com>
2019-01-30 16:12:05 -0500

    bufix: issue 398

--- src/MUIDataTable.js
+++ src/MUIDataTable.js
@@ -152,7 +152,11 @@ class MUIDataTable extends React.Component {
     this.headCellRefs = {};
     this.setHeadResizeable = () => {};
     this.updateDividers = () => {};
-    window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
+
+    if (window) {
+      window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
+    }
+
   }
 
   componentWillMount() {
