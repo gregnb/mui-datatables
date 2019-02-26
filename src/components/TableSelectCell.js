37b69dfec9cb2888cf2538e8e3c38e13dfd37f3c
Nihey Takizawa <nihey.takizawa@gmail.com>
2019-02-26 16:06:12 -0300

    Add a missing import on TableSelectCell

--- src/components/TableSelectCell.js
+++ src/components/TableSelectCell.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Checkbox from '@material-ui/core/Checkbox';
+import Radio from '@material-ui/core/Radio';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
