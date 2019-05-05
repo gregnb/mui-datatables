c873da9e91ae1936776bbe853873dd81d4e0fb07
Benny Zemmour <benny.zemmour@gmail.com>
2019-05-05 23:19:35 +0300

    additional tests

--- test/MUIDataTableFilter.test.js
+++ test/MUIDataTableFilter.test.js
@@ -9,6 +9,7 @@ import TextField from '@material-ui/core/TextField';
 import TableFilter from '../src/components/TableFilter';
 import Typography from '@material-ui/core/Typography';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const Levels = ['Low', 'Normal', 'High'];
 
@@ -300,13 +301,70 @@ describe('<TableFilter />', function() {
             const actualResult = shallowWrapper
                 .find(FormControlLabel)
                 .map(x => x.prop('label'))
-                .filter(label => Levels.includes(label))
+                .filter(label => Levels.includes(label));
 
             assert.strictEqual(actualResult.length, 3);
             assert.strictEqual(actualResult[0], Levels[0]);
             assert.strictEqual(actualResult[1], Levels[1]);
             assert.strictEqual(actualResult[2], Levels[2]);
         });
-    });
 
+      it('test that all select display the right content using customFilterListRender', () => {
+        const options = { filterType: 'select', textLabels };
+        const filterList = [[], [], [], [], []];
+        const onFilterUpdate = spy();
+
+        const shallowWrapper = shallow(
+            <TableFilter
+                columns={columns}
+                onFilterUpdate={onFilterUpdate}
+                filterData={filterData}
+                filterList={filterList}
+                options={options}
+            />,
+        ).dive();
+
+        const selectLevel = shallowWrapper
+            .find(Select)
+            .last();
+
+        assert.strictEqual(selectLevel.prop('name'), 'level');
+
+        const renderedOptions = selectLevel
+            .children()
+            .children()
+            .map(x=> x.text());
+
+        assert.sameOrderedMembers(renderedOptions, ['All'].concat(Levels));
+
+      });
+
+      it('test that all multiselect display the right content using customFilterListRender', () => {
+        const options = { filterType: 'multiselect', textLabels };
+        const filterList = [[], [], [], [], []];
+        const onFilterUpdate = spy();
+
+        const shallowWrapper = shallow(
+            <TableFilter
+                columns={columns}
+                onFilterUpdate={onFilterUpdate}
+                filterData={filterData}
+                filterList={filterList}
+                options={options}
+            />,
+        ).dive();
+
+        const selectLevel = shallowWrapper
+            .find(Select)
+            .last();
+
+        assert.strictEqual(selectLevel.prop('name'), 'level');
+
+        const renderedOptions = selectLevel
+            .find(ListItemText)
+            .map(childs => childs.prop('primary'));
+
+        assert.sameOrderedMembers(renderedOptions, Levels);
+      });
+    });
 });
