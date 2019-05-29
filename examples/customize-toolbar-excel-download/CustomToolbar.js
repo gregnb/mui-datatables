61e418e18d6c39103ddf90d45324808f3f7c2a2a
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-05-29 02:26:08 -0400

    Revert "remove xls download example"
    This reverts commit f1f4e8dc6be850e98d82757dfc8d0edd5f63299e.

---
+++ examples/customize-toolbar-excel-download/CustomToolbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
+
+const CustomToolbar = props => {
+  return (
+    <span>
+      <Tooltip title={'XLS Download'}>
+        <IconButton onClick={props.handleXLSXDownload}>
+          <CloudDownloadIcon />
+        </IconButton>
+      </Tooltip>
+    </span>
+  );
+};
+
+export default CustomToolbar;
