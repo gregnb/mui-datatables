76f4aa8909502d7022314c2449cd38a038f1fada
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-05-29 19:02:11 -0400

    Improve grid spacing in filters dialog (#640)
    * Improve grid spacing in filters dialog
    
    In particular, the way margins were set, we had asymetry on the second column as well as a waste of space
    
    * Tweak height and padding

--- src/components/TableFilter.js
+++ src/components/TableFilter.js
@@ -18,7 +18,7 @@ import { TextField, Grid, GridList, GridListTile } from '@material-ui/core';
 export const defaultFilterStyles = theme => ({
   root: {
     backgroundColor: theme.palette.background.default,
-    padding: '16px 24px 16px 24px',
+    padding: '24px 24px 36px 24px',
     fontFamily: 'Roboto',
   },
   header: {
@@ -90,9 +90,7 @@ export const defaultFilterStyles = theme => ({
     justifyContent: 'space-between',
   },
   selectFormControl: {
-    flex: '1 1 calc(50% - 24px)',
-    marginRight: '24px',
-    marginBottom: '24px',
+    flex: '1 1 calc(50% - 24px)'
   },
   /* textField */
   textFieldRoot: {
@@ -104,8 +102,6 @@ export const defaultFilterStyles = theme => ({
   },
   textFieldFormControl: {
     flex: '1 1 calc(50% - 24px)',
-    marginRight: '24px',
-    marginBottom: '24px',
   },
 });
 
@@ -295,7 +291,7 @@ class TableFilter extends React.Component {
           </div>
           <div className={classes.filtersSelected} />
         </div>
-        <GridList cellHeight="auto" cols={filterGridColumns}>
+        <GridList cellHeight="auto" cols={filterGridColumns} cellHeight={64} spacing={34}>
           {columns.map((column, index) => {
             if (column.filter) {
               const filterType = column.filterType || options.filterType;
