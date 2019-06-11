2e58b58d076833633c07d990853ff3c20669f70b
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-06-11 00:34:01 -0400

    Fix filter dialog height for checkboxes (#684)

--- src/components/TableFilter.js
+++ src/components/TableFilter.js
@@ -291,7 +291,7 @@ class TableFilter extends React.Component {
           </div>
           <div className={classes.filtersSelected} />
         </div>
-        <GridList cellHeight="auto" cols={filterGridColumns} cellHeight={64} spacing={34}>
+        <GridList cellHeight="auto" cols={filterGridColumns} cellHeight={70} spacing={34}>
           {columns.map((column, index) => {
             if (column.filter) {
               const filterType = column.filterType || options.filterType;
