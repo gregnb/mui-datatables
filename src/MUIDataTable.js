a3aead8a1a08e47037148315dc089b957a52acc6
John Detlefs <jdetle@gmail.com>
2019-06-19 20:33:13 -0500

    Undo any deep overwriting of array (#668)

--- src/MUIDataTable.js
+++ src/MUIDataTable.js
@@ -274,6 +274,9 @@ class MUIDataTable extends React.Component {
       extra.selectableRows = props.options.selectableRows ? 'multiple' : 'none';
     }
     this.options = merge(defaultOptions, props.options, extra);
+    if (props.options.rowsPerPageOptions) {
+      this.options.rowsPerPageOptions = props.options.rowsPerPageOptions;
+    }
   }
 
   validateOptions(options) {
@@ -1185,6 +1188,7 @@ class MUIDataTable extends React.Component {
           options={this.options}
           page={page}
           rowCount={rowCount}
+          rowsPerPageOptions={this.options.rowsPerPageOptions}
           rowsPerPage={rowsPerPage}
           changeRowsPerPage={this.changeRowsPerPage}
           changePage={this.changePage}
