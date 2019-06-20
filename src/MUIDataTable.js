b4e1ca45f23776b724ff6e3de2a62c5be3c3c7a8
Diazole <sean.godsell@gmail.com>
2019-06-20 02:32:51 +0100

    Move the column argument to the correct position (#675)

--- src/MUIDataTable.js
+++ src/MUIDataTable.js
@@ -428,7 +428,7 @@ class MUIDataTable extends React.Component {
         }
 
         if (typeof column.customBodyRender === 'function') {
-          const tableMeta = this.getTableMeta(rowIndex, colIndex, value, [], column, this.state);
+          const tableMeta = this.getTableMeta(rowIndex, colIndex, value, column, [], this.state);
           const funcResult = column.customBodyRender(value, tableMeta);
 
           if (React.isValidElement(funcResult) && funcResult.props.value) {
