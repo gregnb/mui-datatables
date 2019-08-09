82e343e266f21ce0e158a9ae1638e945e24f95a8
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-08-09 01:31:58 -0400

    Bugfix/issue 817 fix rows selected (#818)
    * Add failing test for selected rows when no option is specified
    
    * Fix issue with using default options when not specified
    
    * Add deprecation warning for booleans used in selectableRows
    
    * Add test for selectableRows single option
    
    * Add test for selectableRows none option

--- test/MUIDataTable.test.js
+++ test/MUIDataTable.test.js
@@ -815,9 +815,50 @@ describe('<MUIDataTable />', function() {
     assert.deepEqual(state.selectedRows.data, expectedResult);
   });
 
+  it('should not update selectedRows when using rowsSelected option with type=none', () => {
+    const options = {
+      selectableRows: 'none',
+      rowsSelected: [0],
+    };
+    const shallowWrapper = shallow(<MUIDataTable columns={columns} data={data} options={options} />).dive();
+    const instance = shallowWrapper.instance();
+
+    const state = shallowWrapper.state();
+    const expectedResult = [];
+
+    assert.deepEqual(state.selectedRows.data, expectedResult);
+  });
+
+  it('should update selectedRows when using rowsSelected option with type=single', () => {
+    const options = {
+      selectableRows: 'single',
+      rowsSelected: [0],
+    };
+    const shallowWrapper = shallow(<MUIDataTable columns={columns} data={data} options={options} />).dive();
+    const instance = shallowWrapper.instance();
+
+    const state = shallowWrapper.state();
+    const expectedResult = [{ index: 0, dataIndex: 0 }];
+
+    assert.deepEqual(state.selectedRows.data, expectedResult);
+  });
+
   it('should update selectedRows when using rowsSelected option with type=multiple', () => {
     const options = {
       selectableRows: 'multiple',
+      rowsSelected: [0, 3],
+    };
+    const shallowWrapper = shallow(<MUIDataTable columns={columns} data={data} options={options} />).dive();
+    const instance = shallowWrapper.instance();
+
+    const state = shallowWrapper.state();
+    const expectedResult = [{ index: 0, dataIndex: 0 }, { index: 3, dataIndex: 3 }];
+
+    assert.deepEqual(state.selectedRows.data, expectedResult);
+  });
+
+  it('should update selectedRows (with default type=multiple option) when using rowsSelected with no option specified', () => {
+    const options = {
       rowsSelected: [0, 3],
     };
     const shallowWrapper = shallow(<MUIDataTable columns={columns} data={data} options={options} />).dive();
