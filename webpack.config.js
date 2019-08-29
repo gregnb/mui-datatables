8c919c20fa1afd91aeffc8cbdd1434f6a983bb32
Ali Elkhateeb <ali.elkhateeb1990@gmail.com>
2019-08-29 09:43:03 +0800

    #447-Search box - Default Text
    Deleted examples/customize-search-placeholder
    Added searchPlaceholder to examples/customize-search instead

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    app: "./examples/customize-search-placeholder/index.js"
+    app: "./examples/customize-search/index.js"
   },
   stats: "verbose",
   context: __dirname,
