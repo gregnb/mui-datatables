0cc2ddfa449216ac9b22f2df47926407d71d4c30
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-09-10 20:58:17 -0400

    Add test for onFilterDialogClose

--- test/MUIDataTableToolbar.test.js
+++ test/MUIDataTableToolbar.test.js
@@ -196,6 +196,22 @@ describe('<TableToolbar />', function() {
     assert.strictEqual(onFilterDialogOpen.callCount, 1);
   });
 
+  it('should call onFilterDialogClose when closing filters dialog', () => {
+    const onFilterDialogClose = spy();
+    const newOptions = { ...options, onFilterDialogClose };
+    const shallowWrapper = shallow(
+      <TableToolbar columns={columns} data={data} options={newOptions} setTableAction={setTableAction} />,
+    ).dive();
+    const instance = shallowWrapper.instance();
+
+    instance.setActiveIcon('filter');
+    shallowWrapper.update();
+    instance.setActiveIcon(undefined);
+    shallowWrapper.update();
+
+    assert.strictEqual(onFilterDialogClose.callCount, 1);
+  });
+
   it('should set icon when calling method setActiveIcon', () => {
     const shallowWrapper = shallow(
       <TableToolbar columns={columns} data={data} options={options} setTableAction={setTableAction} />,
