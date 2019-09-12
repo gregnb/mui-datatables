97a639f7042016bb838a76f381ca7b68eded35e7
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-09-12 04:07:52 -0400

    Prettify files for release

--- src/utils.js
+++ src/utils.js
@@ -17,8 +17,8 @@ function getCollatorComparator() {
 
 function sortCompare(order) {
   return (a, b) => {
-    var aData = (a.data === null || typeof a.data === 'undefined') ? '' : a.data;
-    var bData = (b.data === null || typeof b.data === 'undefined') ? '' : b.data;
+    var aData = a.data === null || typeof a.data === 'undefined' ? '' : a.data;
+    var bData = b.data === null || typeof b.data === 'undefined' ? '' : b.data;
     return (
       (typeof aData.localeCompare === 'function' ? aData.localeCompare(bData) : aData - bData) *
       (order === 'asc' ? 1 : -1)
