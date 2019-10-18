80262056e5552ca209f509bcc331452f2df5f1f6
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-10-18 19:50:42 -0400

    Change utils test to use nested describe blocks
    This will help us distinguish complex functions within the utils file

--- test/utils.test.js
+++ test/utils.test.js
@@ -2,47 +2,59 @@ import { getPageValue } from '../src/utils';
 import { assert } from 'chai';
 
 describe('utils.js', () => {
-  it('returns the highest in bounds page value when calculating page via getPageValue', () => {
-    // Test when page is out of bounds and count is greater than rowsPerPage
-    let count = 30;
-    let rowsPerPage = 10;
-    let page = 5;
-    let actualResult = getPageValue(count, rowsPerPage, page);
-    assert.strictEqual(actualResult, 2);
+  describe('getPageValue', () => {
+    it('returns the highest in bounds page value when page is out of bounds and count is greater than rowsPerPage', () => {
+      const count = 30;
+      const rowsPerPage = 10;
+      const page = 5;
 
-    // Test when page is in bounds and count is greater than rowsPerPage
-    count = 30;
-    rowsPerPage = 10;
-    page = 1;
-    actualResult = getPageValue(count, rowsPerPage, page);
-    assert.strictEqual(actualResult, 1);
+      const actualResult = getPageValue(count, rowsPerPage, page);
+      assert.strictEqual(actualResult, 2);
+    });
 
-    // Test when page is out of bounds and count is less than rowsPerPage
-    count = 3;
-    rowsPerPage = 10;
-    page = 1;
-    actualResult = getPageValue(count, rowsPerPage, page);
-    assert.strictEqual(actualResult, 0);
+    it('returns the highest in bounds page value when page is in bounds and count is greater than rowsPerPage', () => {
+      const count = 30;
+      const rowsPerPage = 10;
+      const page = 1;
 
-    // Test when page is in bounds and count is less than rowsPerPage
-    count = 3;
-    rowsPerPage = 10;
-    page = 0;
-    actualResult = getPageValue(count, rowsPerPage, page);
-    assert.strictEqual(actualResult, 0);
+      const actualResult = getPageValue(count, rowsPerPage, page);
+      assert.strictEqual(actualResult, 1);
+    });
 
-    // Test when page is out of bounds and count is equal to rowsPerPage
-    count = 10;
-    rowsPerPage = 10;
-    page = 1;
-    actualResult = getPageValue(count, rowsPerPage, page);
-    assert.strictEqual(actualResult, 0);
+    it('returns the highest in bounds page value when page is out of bounds and count is less than rowsPerPage', () => {
+      const count = 3;
+      const rowsPerPage = 10;
+      const page = 1;
 
-    // Test when page is in bounds and count is equal to rowsPerPage
-    count = 10;
-    rowsPerPage = 10;
-    page = 0;
-    actualResult = getPageValue(count, rowsPerPage, page);
-    assert.strictEqual(actualResult, 0);
+      const actualResult = getPageValue(count, rowsPerPage, page);
+      assert.strictEqual(actualResult, 0);
+    });
+
+    it('returns the highest in bounds page value when page is in bounds and count is less than rowsPerPage', () => {
+      const count = 3;
+      const rowsPerPage = 10;
+      const page = 0;
+
+      const actualResult = getPageValue(count, rowsPerPage, page);
+      assert.strictEqual(actualResult, 0);
+    });
+
+    it('returns the highest in bounds page value when page is out of bounds and count is equal to rowsPerPage', () => {
+      const count = 10;
+      const rowsPerPage = 10;
+      const page = 1;
+
+      const actualResult = getPageValue(count, rowsPerPage, page);
+      assert.strictEqual(actualResult, 0);
+    });
+
+    it('returns the highest in bounds page value when page is in bounds and count is equal to rowsPerPage', () => {
+      const count = 10;
+      const rowsPerPage = 10;
+      const page = 0;
+
+      const actualResult = getPageValue(count, rowsPerPage, page);
+      assert.strictEqual(actualResult, 0);
+    });
   });
 });
