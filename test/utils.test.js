d5dfd25a422334cc918bdd2a416757737accfe3b
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-10-18 19:43:23 -0400

    Add assertions for equality cases

--- test/utils.test.js
+++ test/utils.test.js
@@ -3,7 +3,7 @@ import { assert } from 'chai';
 
 describe('utils.js', () => {
   it('returns the highest in bounds page value when calculating page via getPageValue', () => {
-    // Test when page is out of bounds
+    // Test when page is out of bounds and count is greater than rowsPerPage
     let count = 30;
     let rowsPerPage = 10;
     let page = 5;
@@ -11,7 +11,7 @@ describe('utils.js', () => {
     let actualResult = getPageValue(count, rowsPerPage, page);
     assert.strictEqual(actualResult, 2);
 
-    // Test when page is in bounds
+    // Test when page is in bounds and count is greater than rowsPerPage
     count = 30;
     rowsPerPage = 10;
     page = 1;
@@ -29,6 +29,22 @@ describe('utils.js', () => {
 
     // Test when page is in bounds and count is less than rowsPerPage
     count = 3;
+    rowsPerPage = 10;
+    page = 0;
+
+    actualResult = getPageValue(count, rowsPerPage, page);
+    assert.strictEqual(actualResult, 0);
+
+    // Test when page is out of bounds and count is equal to rowsPerPage
+    count = 10;
+    rowsPerPage = 10;
+    page = 1;
+
+    actualResult = getPageValue(count, rowsPerPage, page);
+    assert.strictEqual(actualResult, 0);
+
+    // Test when page is in bounds and count is equal to rowsPerPage
+    count = 10;
     rowsPerPage = 10;
     page = 0;
 
