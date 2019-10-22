63bf751e82c9069f8654cda2bacc65797d2f532b
Try Ajitiono <ballinst@gmail.com>
2019-10-23 09:14:09 +1100

    Add example
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- examples/examples.js
+++ examples/examples.js
@@ -16,6 +16,7 @@ import CustomizeToolbar from "./customize-toolbar";
 import CustomizeToolbarSelect from "./customize-toolbarselect";
 import DataAsObjects from "./data-as-objects";
 import ExpandableRows from "./expandable-rows";
+import ExpandableRowsWithHiddenExpand from "./expandable-rows-hidden-expand";
 import FixedHeader from "./fixed-header";
 import HideColumnsPrint from "./hide-columns-print";
 import OnDownload from "./on-download";
@@ -52,6 +53,7 @@ export default {
     'Customize Toolbar Select': CustomizeToolbarSelect,
     'Data As Objects': DataAsObjects,
     'Expandable Rows': ExpandableRows,
+    'Expandable Rows, Hidden Expand': ExpandableRowsWithHiddenExpand,
     'Fixed Header': FixedHeader,
     'Hide Columns Print': HideColumnsPrint,
     'OnDownload': OnDownload,
