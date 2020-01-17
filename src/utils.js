58df9bcd032da8230fabb2ae3362178f4ec58669
John Detlefs <jdetle@gmail.com>
2020-01-17 01:13:19 -0600

    Deprecation warnings (#1073)
    * Only show deprecation warnings in dev mode.
    
    * Hide deprecation warnings
    
    * Add warn deprecated function to hide unless dev.
    
    * Run prettier, change dep notice, revert text labels change
    
    * Console error

--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,12 @@ function buildMap(rows) {
   }, {});
 }
 
+function warnDeprecated(warning) {
+  if (process.env.NODE_ENV === 'development') {
+    console.error(`Deprecation Notice:  ${warning}`);
+  }
+}
+
 function getPageValue(count, rowsPerPage, page) {
   const totalPages = count <= rowsPerPage ? 1 : Math.ceil(count / rowsPerPage);
 
@@ -108,4 +114,13 @@ function createCSVDownload(columns, data, options, downloadCSV) {
   downloadCSV(csv, options.downloadOptions.filename);
 }
 
-export { buildMap, getPageValue, getCollatorComparator, sortCompare, createCSVDownload, buildCSV, downloadCSV };
+export {
+  buildMap,
+  getPageValue,
+  getCollatorComparator,
+  sortCompare,
+  createCSVDownload,
+  buildCSV,
+  downloadCSV,
+  warnDeprecated,
+};
