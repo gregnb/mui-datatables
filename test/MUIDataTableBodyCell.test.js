3f6807564ac319e666b856c025b48e0faf183b26
Patrick Gillespie <patorjk@gmail.com>
2020-06-06 18:26:09 -0400

    made test a little more robust

--- test/MUIDataTableBodyCell.test.js
+++ test/MUIDataTableBodyCell.test.js
@@ -25,16 +25,20 @@ describe('<TableBodyCell />', function() {
     data = [
       ['Joe James', 'Test Corp', 'Yonkers', 'NY'],
       ['John Walsh', 'Test Corp', 'Hartford', null],
-      ['Bob Herm', 'Test Corp', 'Tampa', 'FL'],
+      ['Bob Herm', 'Test Corp X', 'Tampa', 'FL'],
       ['James Houston', 'Test Corp', 'Dallas', 'TX'],
     ];
   });
 
   it('should execute "onCellClick" prop when clicked if provided', () => {
     var clickCount = 0;
+    var colIndex, rowIndex, colData;
     const options = {
-      onCellClick: () => {
+      onCellClick: (val, colMeta) => {
         clickCount++;
+        colIndex = colMeta.colIndex;
+        rowIndex = colMeta.rowIndex;
+        colData = val;
       }
     };
 
@@ -45,7 +49,29 @@ describe('<TableBodyCell />', function() {
       .at(0)
       .simulate('click');
     assert.strictEqual(clickCount, 1);
+    assert.strictEqual(colIndex, 0);
+    assert.strictEqual(rowIndex, 0);
+    assert.strictEqual(colData, 'Joe James');
+
+    fullWrapper
+      .find('[data-testid="MuiDataTableBodyCell-2-3"]')
+      .at(0)
+      .simulate('click');
+    assert.strictEqual(clickCount, 2);
+    assert.strictEqual(colIndex, 2);
+    assert.strictEqual(rowIndex, 3);
+    assert.strictEqual(colData, 'Dallas');
+
+    fullWrapper
+      .find('[data-testid="MuiDataTableBodyCell-1-2"]')
+      .at(0)
+      .simulate('click');
+    assert.strictEqual(clickCount, 3);
+    assert.strictEqual(colIndex, 1);
+    assert.strictEqual(rowIndex, 2);
+    assert.strictEqual(colData, 'Test Corp X');
 
   });
 
 });
+
