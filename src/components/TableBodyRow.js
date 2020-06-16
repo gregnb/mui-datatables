dd85ef6916b66597002376bf9e92a9a500bc7c30
do-woo <wdh2100@gmail.com>
2020-06-16 22:34:30 +0900

    Refactoring TableBodyRow

--- src/components/TableBodyRow.js
+++ src/components/TableBodyRow.js
@@ -2,83 +2,83 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import TableRow from '@material-ui/core/TableRow';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
-const defaultBodyRowStyles = theme => ({
-  root: {
-    // material v4
-    '&.Mui-selected': {
-      backgroundColor: theme.palette.action.selected,
+const useStyles = makeStyles(
+  theme => ({
+    root: {
+      // material v4
+      '&.Mui-selected': {
+        backgroundColor: theme.palette.action.selected,
+      },
+      // material v3 workaround
+      '&.mui-row-selected': {
+        backgroundColor: theme.palette.action.selected,
+      },
     },
-
-    // material v3 workaround
-    '&.mui-row-selected': {
-      backgroundColor: theme.palette.action.selected,
+    hoverCursor: { cursor: 'pointer' },
+    responsiveStacked: {
+      [theme.breakpoints.down('sm')]: {
+        borderTop: 'solid 2px rgba(0, 0, 0, 0.15)',
+        borderBottom: 'solid 2px rgba(0, 0, 0, 0.15)',
+        padding: 0,
+        margin: 0,
+      },
     },
-  },
-  hoverCursor: { cursor: 'pointer' },
-  responsiveStacked: {
-    [theme.breakpoints.down('sm')]: {
-      borderTop: 'solid 2px rgba(0, 0, 0, 0.15)',
-      borderBottom: 'solid 2px rgba(0, 0, 0, 0.15)',
-      padding: 0,
-      margin: 0,
+    responsiveSimple: {
+      [theme.breakpoints.down('xs')]: {
+        borderTop: 'solid 2px rgba(0, 0, 0, 0.15)',
+        borderBottom: 'solid 2px rgba(0, 0, 0, 0.15)',
+        padding: 0,
+        margin: 0,
+      },
     },
-  },
-  responsiveSimple: {
-    [theme.breakpoints.down('xs')]: {
-      borderTop: 'solid 2px rgba(0, 0, 0, 0.15)',
-      borderBottom: 'solid 2px rgba(0, 0, 0, 0.15)',
-      padding: 0,
-      margin: 0,
-    },
-  },
-});
+  }),
+  { name: 'MUIDataTableBodyRow' },
+);
 
-class TableBodyRow extends React.Component {
-  static propTypes = {
-    /** Options used to describe table */
-    options: PropTypes.object.isRequired,
-    /** Callback to execute when row is clicked */
-    onClick: PropTypes.func,
-    /** Current row selected or not */
-    rowSelected: PropTypes.bool,
-    /** Extend the style applied to components */
-    classes: PropTypes.object,
-  };
+const TableBodyRow = ({ options, rowSelected, onClick, className, isRowSelectable, children, ...otherProps }) => {
+  const classes = useStyles();
+  const methods = {};
 
-  render() {
-    const { classes, options, rowSelected, onClick, className, isRowSelectable, ...rest } = this.props;
-
-    var methods = {};
-    if (onClick) {
-      methods.onClick = onClick;
-    }
-
-    return (
-      <TableRow
-        hover={options.rowHover ? true : false}
-        {...methods}
-        className={classNames(
-          {
-            [classes.root]: true,
-            [classes.hover]: options.rowHover,
-            [classes.hoverCursor]: (options.selectableRowsOnClick && isRowSelectable) || options.expandableRowsOnClick,
-            [classes.responsiveSimple]: options.responsive === 'simple',
-            [classes.responsiveStacked]:
-              options.responsive === 'vertical' ||
-              options.responsive === 'stacked' ||
-              options.responsive === 'stackedFullWidth',
-            'mui-row-selected': rowSelected,
-          },
-          className,
-        )}
-        selected={rowSelected}
-        {...rest}>
-        {this.props.children}
-      </TableRow>
-    );
+  if (onClick) {
+    methods.onClick = onClick;
   }
-}
 
-export default withStyles(defaultBodyRowStyles, { name: 'MUIDataTableBodyRow' })(TableBodyRow);
+  return (
+    <TableRow
+      hover={options.rowHover === true}
+      {...methods}
+      className={classNames(
+        {
+          [classes.root]: true,
+          [classes.hover]: options.rowHover,
+          [classes.hoverCursor]: (options.selectableRowsOnClick && isRowSelectable) || options.expandableRowsOnClick,
+          [classes.responsiveSimple]: options.responsive === 'simple',
+          [classes.responsiveStacked]:
+            options.responsive === 'vertical' ||
+            options.responsive === 'stacked' ||
+            options.responsive === 'stackedFullWidth',
+          'mui-row-selected': rowSelected,
+        },
+        className,
+      )}
+      selected={rowSelected}
+      {...otherProps}>
+      {children}
+    </TableRow>
+  );
+};
+
+TableBodyRow.propTypes = {
+  /** Options used to describe table */
+  options: PropTypes.object.isRequired,
+  /** Callback to execute when row is clicked */
+  onClick: PropTypes.func,
+  /** Current row selected or not */
+  rowSelected: PropTypes.bool,
+  /** Table Row */
+  children: PropTypes.node,
+};
+
+export default TableBodyRow;
