f35fb54610f734989cbb8cbe28c7d52315e27a77
do-woo <wdh2100@gmail.com>
2020-06-16 22:38:44 +0900

    Refactoring TableToolbarSelect

--- src/components/TableToolbarSelect.js
+++ src/components/TableToolbarSelect.js
@@ -4,49 +4,45 @@ import Paper from '@material-ui/core/Paper';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import DeleteIcon from '@material-ui/icons/Delete';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import MuiTooltip from '@material-ui/core/Tooltip';
 
-const defaultToolbarSelectStyles = theme => ({
-  root: {
-    backgroundColor: theme.palette.background.default,
-    flex: '1 1 100%',
-    display: 'flex',
-    position: 'relative',
-    zIndex: 120,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingTop: typeof theme.spacing === 'function' ? theme.spacing(1) : theme.spacing.unit,
-    paddingBottom: typeof theme.spacing === 'function' ? theme.spacing(1) : theme.spacing.unit,
-    '@media print': {
-      display: 'none',
+const useStyles = makeStyles(
+  theme => ({
+    root: {
+      backgroundColor: theme.palette.background.default,
+      flex: '1 1 100%',
+      display: 'flex',
+      position: 'relative',
+      zIndex: 120,
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      paddingTop: typeof theme.spacing === 'function' ? theme.spacing(1) : theme.spacing.unit,
+      paddingBottom: typeof theme.spacing === 'function' ? theme.spacing(1) : theme.spacing.unit,
+      '@media print': {
+        display: 'none',
+      },
     },
-  },
-  title: {
-    paddingLeft: '26px',
-  },
-  iconButton: {
-    marginRight: '24px',
-  },
-  deleteIcon: {},
-});
+    title: {
+      paddingLeft: '26px',
+    },
+    iconButton: {
+      marginRight: '24px',
+    },
+    deleteIcon: {},
+  }),
+  { name: 'MUIDataTableToolbarSelect' },
+);
 
-class TableToolbarSelect extends React.Component {
-  static propTypes = {
-    /** Options used to describe table */
-    options: PropTypes.object.isRequired,
-    /** Current row selected or not */
-    rowSelected: PropTypes.bool,
-    /** Callback to trigger selected rows delete */
-    onRowsDelete: PropTypes.func,
-    /** Extend the style applied to components */
-    classes: PropTypes.object,
-  };
+const TableToolbarSelect = ({ onRowsDelete, selectedRows, options, displayData, components = {}, selectRowUpdate }) => {
+  const classes = useStyles();
+  const textLabels = options.textLabels.selectedRows;
+  const Tooltip = components.Tooltip || MuiTooltip;
 
   /**
    * @param {number[]} selectedRows Array of rows indexes that are selected, e.g. [0, 2] will select first and third rows in table
    */
-  handleCustomSelectedRows = selectedRows => {
+  const handleCustomSelectedRows = selectedRows => {
     if (!Array.isArray(selectedRows)) {
       throw new TypeError(`"selectedRows" must be an "array", but it's "${typeof selectedRows}"`);
     }
@@ -55,37 +51,41 @@ class TableToolbarSelect extends React.Component {
       throw new TypeError(`Array "selectedRows" must contain only numbers`);
     }
 
-    const { options } = this.props;
     if (selectedRows.length > 1 && options.selectableRows === 'single') {
       throw new Error('Can not select more than one row when "selectableRows" is "single"');
     }
-    this.props.selectRowUpdate('custom', selectedRows);
+    selectRowUpdate('custom', selectedRows);
   };
 
-  render() {
-    const { classes, onRowsDelete, selectedRows, options, displayData, components = {} } = this.props;
-    const textLabels = options.textLabels.selectedRows;
-    const Tooltip = components.Tooltip || MuiTooltip;
+  return (
+    <Paper className={classes.root}>
+      <div>
+        <Typography variant="subtitle1" className={classes.title}>
+          {selectedRows.data.length} {textLabels.text}
+        </Typography>
+      </div>
+      {options.customToolbarSelect ? (
+        options.customToolbarSelect(selectedRows, displayData, handleCustomSelectedRows)
+      ) : (
+        <Tooltip title={textLabels.delete}>
+          <IconButton className={classes.iconButton} onClick={onRowsDelete} aria-label={textLabels.deleteAria}>
+            <DeleteIcon className={classes.deleteIcon} />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Paper>
+  );
+};
 
-    return (
-      <Paper className={classes.root}>
-        <div>
-          <Typography variant="subtitle1" className={classes.title}>
-            {selectedRows.data.length} {textLabels.text}
-          </Typography>
-        </div>
-        {options.customToolbarSelect ? (
-          options.customToolbarSelect(selectedRows, displayData, this.handleCustomSelectedRows)
-        ) : (
-          <Tooltip title={textLabels.delete}>
-            <IconButton className={classes.iconButton} onClick={onRowsDelete} aria-label={textLabels.deleteAria}>
-              <DeleteIcon className={classes.deleteIcon} />
-            </IconButton>
-          </Tooltip>
-        )}
-      </Paper>
-    );
-  }
-}
+TableToolbarSelect.propTypes = {
+  /** Options used to describe table */
+  options: PropTypes.object.isRequired,
+  /** Current row selected or not */
+  rowSelected: PropTypes.bool,
+  /** Callback to trigger selected rows delete */
+  onRowsDelete: PropTypes.func,
+  /** Extend the style applied to components */
+  classes: PropTypes.object,
+};
 
-export default withStyles(defaultToolbarSelectStyles, { name: 'MUIDataTableToolbarSelect' })(TableToolbarSelect);
+export default TableToolbarSelect;
