85ca5e9a0cc5b8c828e01f0189e33e2f5e6fd1ba
patorjk <patorjk@gmail.com>
2020-06-21 21:55:51 -0400

    Revert "Refactoring TableHead"

--- src/components/TableHeadCell.js
+++ src/components/TableHeadCell.js
@@ -1,107 +1,104 @@
-import classNames from 'classnames';
-import HelpIcon from '@material-ui/icons/Help';
-import MuiTooltip from '@material-ui/core/Tooltip';
-import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import TableCell from '@material-ui/core/TableCell';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
+import HelpIcon from '@material-ui/icons/Help';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import React, {useState,useEffect} from 'react';
+import MuiTooltip from '@material-ui/core/Tooltip';
+import {useDrop, useDrag} from 'react-dnd';
 import useColumnDrop from '../hooks/useColumnDrop.js';
-import { makeStyles } from '@material-ui/core/styles';
-import { useDrag } from 'react-dnd';
 
-const useStyles = makeStyles(
-  theme => ({
-    root: {},
-    fixedHeader: {
-      position: 'sticky',
-      top: '0px',
-      zIndex: 100,
-      backgroundColor: theme.palette.background.paper,
+const useStyles = makeStyles(theme => ({
+  root: {},
+  fixedHeader: {
+    position: 'sticky',
+    top: '0px',
+    zIndex: 100,
+    backgroundColor: theme.palette.background.paper,
+  },
+  tooltip: {
+    cursor: 'pointer',
+  },
+  mypopper: {
+    '&[data-x-out-of-boundaries]': {
+      display: 'none',
     },
-    tooltip: {
-      cursor: 'pointer',
-    },
-    mypopper: {
-      '&[data-x-out-of-boundaries]': {
-        display: 'none',
-      },
-    },
-    data: {
-      display: 'inline-block',
-    },
-    sortAction: {
-      display: 'flex',
-      verticalAlign: 'top',
-      cursor: 'pointer',
-    },
-    dragCursor: {
-      cursor: 'grab',
-    },
-    sortLabelRoot: {
-      height: '10px',
-    },
-    sortActive: {
-      color: theme.palette.text.primary,
-    },
-    toolButton: {
-      display: 'flex',
-      outline: 'none',
-      cursor: 'pointer',
-    },
-    hintIconAlone: {
-      marginTop: '-3px',
-      marginLeft: '3px',
-    },
-    hintIconWithSortIcon: {
-      marginTop: '-3px',
-    },
-  }),
-  { name: 'MUIDataTableHeadCell' },
-);
+  },
+  data: {
+    display: 'inline-block',
+  },
+  sortAction: {
+    display: 'flex',
+    verticalAlign: 'top',
+    cursor: 'pointer',
+  },
+  dragCursor: {
+    cursor: 'grab'
+  },
+  sortLabelRoot: {
+    height: '10px',
+  },
+  sortActive: {
+    color: theme.palette.text.primary,
+  },
+  toolButton: {
+    display: 'flex',
+    outline: 'none',
+    cursor: 'pointer',
+  },
+  hintIconAlone: {
+    marginTop: '-3px',
+    marginLeft: '3px',
+  },
+  hintIconWithSortIcon: {
+    marginTop: '-3px',
+  },
+}), { name: 'MUIDataTableHeadCell'});
 
-const TableHeadCell = ({
-  cellHeaderProps = {},
-  children,
-  colPosition,
-  column,
-  columnOrder = [],
-  components = {},
-  draggableHeadCellRefs,
-  draggingHook,
-  hint,
-  index,
-  options,
-  print,
-  setCellRef,
-  sort,
-  sortDirection,
-  tableRef,
-  timers,
-  toggleSort,
-  updateColumnOrder,
-}) => {
+function TableHeadCell(props) {
+
   const [sortTooltipOpen, setSortTooltipOpen] = useState(false);
   const [hintTooltipOpen, setHintTooltipOpen] = useState(false);
-
+  
   const classes = useStyles();
 
-  const handleKeyboardSortInput = e => {
+  const handleKeyboardSortinput = e => {
     if (e.key === 'Enter') {
-      toggleSort(index);
+      props.toggleSort(props.index);
     }
 
     return false;
   };
 
   const handleSortClick = () => {
-    toggleSort(index);
+    props.toggleSort(props.index);
   };
 
-  const [dragging, setDragging] = draggingHook ? draggingHook : [];
+  const {
+    children,
+    options,
+    sortDirection,
+    sort,
+    hint,
+    index,
+    colPosition,
+    print,
+    column,
+    updateColumnOrder,
+    columnOrder = [],
+    setCellRef,
+    cellHeaderProps = {},
+    draggableHeadCellRefs,
+    tableRef,
+    timers,
+    components = {},
+  } = props;
+  const [dragging, setDragging] = props.draggingHook ? props.draggingHook : [];
 
   const { className, ...otherProps } = cellHeaderProps;
   const Tooltip = components.Tooltip || MuiTooltip;
-  const sortActive = sortDirection !== 'none' && sortDirection !== undefined;
+  const sortActive = sortDirection !== 'none' && sortDirection !== undefined ? true : false;
   const ariaSortDirection = sortDirection === 'none' ? false : sortDirection;
 
   const sortLabelProps = {
@@ -111,12 +108,12 @@ const TableHeadCell = ({
     ...(ariaSortDirection ? { direction: sortDirection } : {}),
   };
 
-  const [{ opacity }, dragRef, preview] = useDrag({
+  const [{opacity}, dragRef, preview] = useDrag({
     item: {
       type: 'HEADER',
-      colIndex: index,
+      colIndex: index
     },
-    begin: monitor => {
+    begin: (monitor) => {
       setHintTooltipOpen(false);
       setSortTooltipOpen(false);
       setDragging(true);
@@ -129,7 +126,7 @@ const TableHeadCell = ({
       return {
         opacity: monitor.isDragging() ? 1 : 0,
       };
-    },
+    }
   });
 
   const [drop] = useColumnDrop({
@@ -144,12 +141,11 @@ const TableHeadCell = ({
     columnOrder,
     transitionTime: options.draggableColumns ? options.draggableColumns.transitionTime : 300,
     tableRef: tableRef ? tableRef() : null,
-    timers,
+    timers
   });
 
   const isDraggingEnabled = () => {
-    if (!draggingHook) return false;
-
+    if (!props.draggingHook) return false;
     return options.draggableColumns && options.draggableColumns.enabled && column.draggable !== false;
   };
 
@@ -165,12 +161,18 @@ const TableHeadCell = ({
     setHintTooltipOpen(true);
   };
 
+  let refProp = {};
+  refProp.ref = el => {
+    drop(el);
+    setCellRef && setCellRef(index + 1, colPosition + 1, el);
+  };
+  
   const getTooltipTitle = () => {
     if (dragging) return '';
     if (!options.textLabels) return '';
     return options.textLabels.body.columnHeaderTooltip
-      ? options.textLabels.body.columnHeaderTooltip(column)
-      : options.textLabels.body.toolTip;
+                ? options.textLabels.body.columnHeaderTooltip(column)
+                : options.textLabels.body.toolTip;
   };
 
   const closeTooltip = () => {
@@ -179,31 +181,21 @@ const TableHeadCell = ({
   };
 
   return (
-    <TableCell
-      ref={ref => {
-        drop(ref);
-        setCellRef && setCellRef(index + 1, colPosition + 1, ref);
-      }}
-      className={cellClass}
-      scope={'col'}
-      sortDirection={ariaSortDirection}
-      data-colindex={index}
-      onMouseDown={closeTooltip}
-      {...otherProps}>
+    <TableCell className={cellClass} scope={'col'} sortDirection={ariaSortDirection} data-colindex={index} {...refProp} onMouseDown={closeTooltip} {...otherProps}>
       {options.sort && sort ? (
         <span
           role="button"
-          onKeyUp={handleKeyboardSortInput}
+          onKeyUp={handleKeyboardSortinput}
           onClick={handleSortClick}
           className={classes.toolButton}
-          data-testid={`headcol-${index}`}
+          data-testid={'headcol-' + props.index}
           ref={isDraggingEnabled() ? dragRef : null}
           tabIndex={0}>
           <Tooltip
             title={getTooltipTitle()}
             placement={'bottom-start'}
             open={sortTooltipOpen}
-            onOpen={() => (dragging ? setSortTooltipOpen(false) : setSortTooltipOpen(true))}
+            onOpen={() => dragging ? setSortTooltipOpen(false) : setSortTooltipOpen(true)}
             onClose={() => setSortTooltipOpen(false)}
             classes={{
               tooltip: classes.tooltip,
@@ -214,7 +206,7 @@ const TableHeadCell = ({
                 className={classNames({
                   [classes.data]: true,
                   [classes.sortActive]: sortActive,
-                  [classes.dragCursor]: isDraggingEnabled,
+                  [classes.dragCursor]: isDraggingEnabled
                 })}>
                 {children}
               </div>
@@ -254,9 +246,11 @@ const TableHeadCell = ({
       )}
     </TableCell>
   );
-};
+}
 
 TableHeadCell.propTypes = {
+  /** Extend the style applied to components */
+  classes: PropTypes.object,
   /** Options used to describe table */
   options: PropTypes.object.isRequired,
   /** Current sort direction */
