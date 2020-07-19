7674d29b00740c134537e8d61b16bf88d7a43277
Patrick Gillespie <patorjk@gmail.com>
2020-07-19 12:34:15 -0400

    test coverage update

--- test/UseColumnDrop.test.js
+++ test/UseColumnDrop.test.js
@@ -58,6 +58,39 @@ describe('useColumnDrop', function() {
     expect(newModel[1].columnIndex).to.equal(0);
   });
 
+  it('should build a column model object when getColModel is called and no select cell exists', () => {
+    let headCellRefs = {
+      0: null,
+      1: {
+        offsetLeft: 0,
+        offsetParent: 0, 
+        offsetWidth: 0, 
+        offsetParent: null,
+      },
+      2: {
+        offsetLeft: 0,
+        offsetParent: 0, 
+        offsetWidth: 0, 
+        offsetParent: null,
+      },
+    };
+    let columnOrder = [0, 1];
+    let columns = [
+      {
+        display: 'true'
+      },
+      {
+        display: 'true'
+      }
+    ];
+
+    let newModel = getColModel(headCellRefs, columnOrder, columns);
+
+    expect(newModel.length).to.equal(2);
+    expect(newModel[0].left).to.equal(0);
+    expect(newModel[1].columnIndex).to.equal(1);
+  });
+
   it('should set columnShift on timers when handleHover is called', () => {
     let offsetParent = {
       offsetLeft: 10,
@@ -119,7 +152,13 @@ describe('useColumnDrop', function() {
       updateColumnOrder: spy(),
       columnOrder: [0, 1],
       transitionTime: 0,
-      tableRef: null,
+      tableRef: {
+        querySelectorAll: () => (
+          [{
+            style: {}
+          }]
+        )
+      },
       tableId: '123',
       timers,
       columns,
