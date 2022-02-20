e2098fc2ecaec74ea02c817bc6eb42fa9da05ed1
Woo Dohyeong <wdh2100@gmail.com>
2022-02-20 21:47:03 +0900

    fix : Changed to work only in browser(storageKey)
    - for nextJS

--- src/localStorage/load.js
+++ src/localStorage/load.js
@@ -1,1 +1,10 @@
-export const load = storageKey => JSON.parse(window.localStorage.getItem(storageKey));
+const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
+
+export const load = storageKey => {
+  if (isBrowser) {
+    return JSON.parse(window.localStorage.getItem(storageKey));
+  } else if (storageKey !== undefined) {
+    console.warn('storageKey support only on browser');
+    return undefined;
+  }
+};
